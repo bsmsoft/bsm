ea56ac007bbf74e921f1602a360eed4d8729ad26
Xianghu Zhao <zhaoxh@ihep.ac.cn>
2017-09-07 12:19:45 +0800

    Remove nullsoft

--- cepcenv/nullsoft/__init__.py
+++
@@ -1,8 +0,0 @@
-from cepcenv.loader import load_common
-
-def load_package(pkg_type):
-    return load_common(pkg_type, 'cepcenv.soft')
-
-def download(download_type, param):
-    downloader = load_common(download_type, 'cepcenv.soft.downloader')(param)
-    downloader.download()
--- cepcenv/nullsoft/ilc.py
+++
@@ -1,7 +0,0 @@
-class Ilc(object):
-    def __init__(self, param):
-        pass
-
-    @property
-    def downloader(self):
-        return ('svn', param)
