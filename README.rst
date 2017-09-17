11af8a63cd8b65e1671eae3124a363121baacc72
Xianghu Zhao <zhaoxh@ihep.ac.cn>
2017-09-17 09:32:03 +0800

    Install arguments could be omitted

--- README.rst
+++ README.rst
@@ -2,3 +2,13 @@ cepcenv
 =======
 
 CEPC software management toolkit
+
+Install `cepcenv` with `curl` or `wget`.
+
+::
+
+    \curl -sSL https://raw.githubusercontent.com/xianghuzhao/cepcenv/master/tool/install.sh | sh -s [INSTALL_DIR]
+
+or::
+
+    \wget -O - https://raw.githubusercontent.com/xianghuzhao/cepcenv/master/tool/install.sh | sh -s [INSTALL_DIR]
--- tool/install.sh
+++ tool/install.sh
@@ -5,9 +5,9 @@ git_repo='https://github.com/xianghuzhao/cepcenv.git'
 
 download_http() {
   if which curl >/dev/null 2>&1; then
-    curl -L $1
+    curl -L "$1"
   elif which wget >/dev/null 2>&1; then
-    wget -O - $1
+    wget -O - "$1"
   else
     echo >&2 "Please install curl or wget first"
     exit 1
@@ -15,35 +15,39 @@ download_http() {
 }
 
 main() {
-  if [ $# -eq 0 ]; then
-    echo >&2 "Please specify the installation directory"
-    exit 2
-  elif [ $# -gt 1 ]; then
+  if [ $# -gt 1 ]; then
     echo >&2 "Only one parameter allowed"
     exit 2
   fi
 
-  install_dir=$1
+  if [ $# -eq 0 ]; then
+    install_dir='.'
+    echo >&2 "Install cepcenv in the current directory..."
+  else
+    install_dir="$1"
+  fi
 
-  mkdir -p $install_dir
+  mkdir -p "$install_dir"
 
-  cd $install_dir
-  install_full_dir=$(pwd)
+  cd "$install_dir"
+  install_full_dir="$(pwd)"
+  echo $install_full_dir
 
   rm -rf pypy*
 
-  download_http $pypy_url | tar xj
+  download_http "$pypy_url" | tar xj
 
   cd pypy*
-  pypy_dir=$(pwd)
+  pypy_dir_name=$(basename "$(pwd)")
   cd ..
+  mv "$pypy_dir_name" pypy
 
-  ${pypy_dir}/bin/pypy -m ensurepip
-  ${pypy_dir}/bin/pip install -U pip wheel
+  pypy/bin/pypy -m ensurepip
+  pypy/bin/pip install -U pip wheel
 
-  ${pypy_dir}/bin/pip install git+${git_repo}
+  pypy/bin/pip install "git+${git_repo}"
 
-  echo "eval \"\$(${pypy_dir}/bin/cepcenv_cmd init)\"" > setup.sh
+  echo "eval \"\$('${install_full_dir}/pypy/bin/cepcenv_cmd' init)\"" > setup.sh
 }
 
 main "$@"
