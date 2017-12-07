35150ddb916334e188fc5ef1e5b3f8c640034f16
Xianghu Zhao <zhaoxh@ihep.ac.cn>
2017-12-07 15:52:51 +0800

    upgrade command output something

--- cepcenv/cmd/upgrade.py
+++ cepcenv/cmd/upgrade.py
@@ -5,7 +5,7 @@ from pip import main
 class Upgrade(object):
     def execute(self, config):
         def pip_install(package):
-            main(['install', '--quiet', '--quiet', '--quiet', '--upgrade', package])
+            main(['install', '--upgrade', package])
 
         pip_install('pip')
         pip_install('cepcenv')
