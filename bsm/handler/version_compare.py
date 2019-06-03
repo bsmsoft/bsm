63ddcc65aee22e0bb79c8c226a2e2943619258c8
Xianghu Zhao <xianghuzhao@gmail.com>
2019-06-03 14:28:32 +0800

    Modify version compare

--- bsm/handler/version_compare.py
+++ bsm/handler/version_compare.py
@@ -1,4 +1,10 @@
-from bsm.util import version_compare
+from packaging import version
 
-def run(ver1, ver2):
-    return version_compare(ver1, ver2)
+def run(pkg1, pkg2):
+    v1 = version.parse(pkg1.get('version', ''))
+    v2 = version.parse(pkg2.get('version', ''))
+    if v1 > v2:
+        return 1
+    if v1 < v2:
+        return -1
+    return 0
