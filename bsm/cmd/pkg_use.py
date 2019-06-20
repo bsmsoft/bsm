ad74a013aa562f0189563856a47808fbdf4f358f
Xianghu Zhao <xianghuzhao@gmail.com>
2019-06-20 17:05:59 +0800

    Output detail of pkg-use

--- bsm/cmd/pkg_use.py
+++ bsm/cmd/pkg_use.py
@@ -1,4 +1,3 @@
-from bsm.cmd import CmdError
 from bsm.cmd.pkg_base import PkgBase
 
 from bsm.logger import get_logger
@@ -11,9 +10,14 @@ class PkgUse(PkgBase):
         category = self._current_category(category)
 
         ctg, sd, ver = self._bsm.use_package(package, category, subdir, version)
-        _logger.info('Load package "{0}" from category "{1}", subdir "{2}", version "{3}"'.format(package, ctg, sd, ver))
+        _logger.info('Load package "{0}" successfully'.format(package))
 
-        pkg_path = self._bsm.package_path(package, category, subdir, version)
-        _logger.info('Package directory: {0}'.format(pkg_path['main_dir']))
+        pkg_path = self._bsm.package_path(package, ctg, sd, ver)
 
-        return ''
+        result = {}
+        result['category'] = ctg
+        result['subdir'] = sd
+        result['name'] = package
+        result['version'] = ver
+        result['main_dir'] = pkg_path['main_dir']
+        return result
